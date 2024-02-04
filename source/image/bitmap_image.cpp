058cc2b261405413891a0a4f4e8c54f4cf559dc5
AndreBe1 <54902170+AndreBe1@users.noreply.github.com>
2024-02-04 22:57:23 +0100

    Update bitmap_image.cpp
    Some random stylistic changes

--- source/image/bitmap_image.cpp
+++ source/image/bitmap_image.cpp
@@ -4,46 +4,22 @@
 
 BitmapImage::BitmapImage(const std::uint32_t image_height, const std::uint32_t image_width)
 	: height{ image_height }, width{ image_width } {
-	if (image_height > 1'024'000) {
-		throw std::exception{ };
-	}
-
-	if (image_height == 0) {
-		throw std::exception{ };
-	}
-
-	if (image_width > 1'024'000) {
-		throw std::exception{ };
-	}
-
-	if (image_width == 0) {
-		throw std::exception{  };
-	}
-
-	pixels.resize(image_height * image_width, BitmapPixel{ 0, 0, 0 });
+	if (image_height > 1'024'000) throw std::exception{};
+	if (image_height == 0) throw std::exception{};
+	if (image_width > 1'024'000) throw std::exception{};
+	if (image_width == 0) throw std::exception{};
+	pixels.resize(image_height * image_width, BitmapPixel{0, 0, 0});
 }
 
 void BitmapImage::set_pixel(const std::uint32_t y_position, const std::uint32_t x_position, const BitmapPixel pixel) {
-	if (y_position >= height) {
-		throw std::exception{  };
-	}
-
-	if (x_position >= width) {
-		throw std::exception{ };
-	}
-
+	if (y_position >= height) throw std::exception{};
+	if (x_position >= width) throw std::exception{};
 	pixels[y_position * width + x_position] = pixel;
 }
 
 BitmapImage::BitmapPixel BitmapImage::get_pixel(const std::uint32_t y_position, const std::uint32_t x_position) const {
-	if (y_position >= height) {
-		throw std::exception{ };
-	}
-
-	if (x_position >= width) {
-		throw std::exception{ };
-	}
-
+	if (y_position >= height) throw std::exception{};
+	if (x_position >= width) throw std::exception{};
 	return pixels[y_position * width + x_position];
 }
 
