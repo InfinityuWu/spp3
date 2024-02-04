d785216ef98459953aa9fec51674237b3700722a
AndreBe1 <54902170+AndreBe1@users.noreply.github.com>
2024-02-04 18:11:01 +0100

    Update Algorithm.cpp
    1b) function body

--- source/encryption/Algorithm.cpp
+++ source/encryption/Algorithm.cpp
@@ -1,1 +1,25 @@
 #include "Algorithm.h"
+
+std::uint64_t Algorithm::encode (const Algorithm::EncryptionScheme& scheme) noexcept {
+  std::uint64_t encoded{0};
+  for (int i = 0; i < 16; i++) {
+    std::uint64_t addend{0};
+    // this switch could be replaced by utilizing associated values within the enum and just assigning them here accordingly
+    switch (scheme[i]) {
+      case E:
+        break;
+      case D:
+        addend = 1;
+        break;
+      case K:
+        addend = 2;
+        break;
+      case T:
+        addend = 3;
+        break;
+    }
+    addend <<= 2 * i;
+    encoded += addend;
+  }
+  return encoded;
+}
