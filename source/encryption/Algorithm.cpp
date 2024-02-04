f3fc78ec9217dc7ca6727e1cd8aaeacf789ed983
AndreBe1 <54902170+AndreBe1@users.noreply.github.com>
2024-02-04 18:37:32 +0100

    Update Algorithm.cpp
    Changed implementation of 1b) such that it now utilizes enum value association

--- source/encryption/Algorithm.cpp
+++ source/encryption/Algorithm.cpp
@@ -3,21 +3,24 @@
 std::uint64_t Algorithm::encode (const Algorithm::EncryptionScheme& scheme) noexcept {
   std::uint64_t encoded{0};
   for (int i = 0; i < 16; i++) {
-    std::uint64_t addend{0};
-    // this switch could be replaced by utilizing associated values within the enum and just assigning them here accordingly
-    switch (scheme[i]) {
-      case E:
-        break;
-      case D:
-        addend = 1;
-        break;
-      case K:
-        addend = 2;
-        break;
-      case T:
-        addend = 3;
-        break;
-    }
+    std::uint64_t addend = static_cast<std::uint64_t>(scheme[i]);
+    /*
+      // this switch is the alternative solution without utilizing associated values within the enum and just assigning them here accordingly
+      std::uint64_t addend{0};
+      switch (scheme[i]) {
+        case Algorithm::EncryptionStep::E:
+          break;
+        case Algorithm::EncryptionStep::D:
+          addend = 1;
+          break;
+        case Algorithm::EncryptionStep::K:
+          addend = 2;
+          break;
+        case Algorithm::EncryptionStep::T:
+          addend = 3;
+          break;
+      }
+    */
     addend <<= 2 * i;
     encoded += addend;
   }
