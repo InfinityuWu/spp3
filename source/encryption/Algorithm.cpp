83a4f3bd88bf6ff05a2c432f6c278e5f879a63e6
AndreBe1 <54902170+AndreBe1@users.noreply.github.com>
2024-02-04 18:51:10 +0100

    Update Algorithm.cpp
    Included implementation for task 1c)

--- source/encryption/Algorithm.cpp
+++ source/encryption/Algorithm.cpp
@@ -1,13 +1,15 @@
 #include "Algorithm.h"
 
+#include <exception>
+
 std::uint64_t Algorithm::encode (const Algorithm::EncryptionScheme& scheme) noexcept {
   std::uint64_t encoded{0};
-  for (int i = 0; i < 16; i++) {
-    std::uint64_t addend = static_cast<std::uint64_t>(scheme[i]);
+  for (int index = 0; index < 16; index++) {
+    std::uint64_t addend = static_cast<std::uint64_t>(scheme[index]);
     /*
       // this switch is the alternative solution without utilizing associated values within the enum and just assigning them here accordingly
       std::uint64_t addend{0};
-      switch (scheme[i]) {
+      switch (scheme[index]) {
         case Algorithm::EncryptionStep::E:
           break;
         case Algorithm::EncryptionStep::D:
@@ -21,8 +23,23 @@ std::uint64_t Algorithm::encode (const Algorithm::EncryptionScheme& scheme) noex
           break;
       }
     */
-    addend <<= 2 * i;
+    addend <<= 2 * index;
     encoded += addend;
   }
   return encoded;
+}
+
+Algorithm::EncryptionScheme decode (const std::uint64_t encoded) {
+  Algorithm::EncryptionScheme scheme{};
+  for (int index = 0; index < 16; index++) {
+    uint64_t first_appearance = (encoded << (62 - 2 * index)) >> 62;
+    uint64_t second_appearance = (encoded << (30 - 2 * index)) >> 62;
+    if (first_appearance != second_appearance) throw std::exception{};
+    if (first_appearance == 0) scheme[index] = Algorithm::EncryptionStep::E;
+    else if (first_appearance == 1) scheme[index] = Algorithm::EncryptionStep::D;
+    else if (first_appearance == 2) scheme[index] = Algorithm::EncryptionStep::K;
+    else if (first_appearance == 3) scheme[index] = Algorithm::EncryptionStep::T;
+    else throw std::exception{};
+  }
+  return scheme;
 }
