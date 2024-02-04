fff324274cf94f18c36a12df8aba901a4c3773fe
AndreBe1 <54902170+AndreBe1@users.noreply.github.com>
2024-02-04 17:54:01 +0100

    Update Algorithm.h
    1a) and parts of 1b) added to Algorithm header file

--- source/encryption/Algorithm.h
+++ source/encryption/Algorithm.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <array>
+#include <cstdint>
+
+class Algorithm {
+
+  public:
+
+    // Task 1a)
+    enum EncryptionStep {E, D, K, T};                        // leave at class scope or put at file scope maybe?
+    using EncryptionScheme = std::array<EncryptionStep,16>;  // alternatively use typedef instead of using for alias
+
+    // Task 1b)
+    [[nodiscard]] static std::uint64_t encode (const EncryptionScheme& scheme) noexcept;
+
+  private:
+
+};
