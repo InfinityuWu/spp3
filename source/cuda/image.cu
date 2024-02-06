7511d1980023b3a74c7f7f7b1e4c4cfb063aad38
AndreBe1 <54902170+AndreBe1@users.noreply.github.com>
2024-02-06 15:46:29 +0100

    Update image.cu

--- source/cuda/image.cu
+++ source/cuda/image.cu
@@ -31,13 +31,18 @@ __global__ void grayscale_kernel (const Pixel<std::uint8_t>* const input, Pixel<
 BitmapImage get_grayscale_cuda (const BitmapImage& source) {
   auto output_image = BitmapImage{source.get_height(), source.get_width()};
   int number_threads_per_block = 16;
+  // creating pointers to be used to work on device (-> GPU) memory
   Pixel<std::uint8_t>* input_gpu;
   Pixel<std::uint8_t>* output_gpu;
+  // allocating device memory of required size dictated by dimensions of the image and having the pointers written to the previously established variables
   cudaMalloc((void**) &input_gpu, source.get_height() * source.get_width() * sizeof(Pixel<std::uint8_t>));
   cudaMalloc((void**) &output_gpu, source.get_height() * source.get_width() * sizeof(Pixel<std::uint8_t>));
+  // copying the entire image pixel data from host (-> CPU) to device as input to the kernel to operate on
   cudaMemcpy(input_gpu, source.get_data(), source.get_height() * source.get_width() * sizeof(Pixel<std::uint8_t>), cudaMemcpyHostToDevice);
   grayscale_kernel<<< {divup(source.get_height(), number_threads_per_block), divup(source.get_width(), number_threads_per_block)}, {number_threads_per_block, number_threads_per_block} >>>(input_gpu, output_gpu, source.get_width(), source.get_height());
+  // copying the entire image pixel data back over from the device to host after the kernel has finished its calculation and has arrived at the desired transformation of the input data
   cudaMemcpy(output_image.get_data(), output_gpu, source.get_height() * source.get_width() * sizeof(Pixel<std::uint8_t>), cudaMemcpyDeviceToHost);
+  // freeing device memory
   cudaFree(input_gpu);
   cudaFree(output_gpu);
   return output_image;
