6cc1043e5305b2211a9f1c205f92747ea40c3aed
Sophie <soso.roggenkamp@gmail.com>
2024-02-06 16:28:12 +0100

    entered names into authors.h

--- source/authors.h
+++ source/authors.h
@@ -1,16 +1,16 @@
 #pragma once
 
-#define AUTHORS_ONE_A ""
-#define AUTHORS_ONE_B ""
-#define AUTHORS_ONE_C ""
-#define AUTHORS_ONE_D ""
+#define AUTHORS_ONE_A "Andre Beer, Julian Meister, Sophie Roggenkamp"
+#define AUTHORS_ONE_B "Andre Beer, Julian Meister, Sophie Roggenkamp"
+#define AUTHORS_ONE_C "Andre Beer, Julian Meister, Sophie Roggenkamp"
+#define AUTHORS_ONE_D "Andre Beer, Julian Meister, Sophie Roggenkamp"
 
-#define AUTHORS_TWO_A ""
-#define AUTHORS_TWO_B ""
-#define AUTHORS_TWO_C ""
+#define AUTHORS_TWO_A "Andre Beer, Julian Meister, Sophie Roggenkamp"
+#define AUTHORS_TWO_B "Andre Beer, Julian Meister, Sophie Roggenkamp"
+#define AUTHORS_TWO_C "Andre Beer, Julian Meister, Sophie Roggenkamp"
 
-#define AUTHORS_THREE_A ""
-#define AUTHORS_THREE_B ""
-#define AUTHORS_THREE_C ""
-#define AUTHORS_THREE_D ""
-#define AUTHORS_THREE_E ""
+#define AUTHORS_THREE_A "Andre Beer, Julian Meister, Sophie Roggenkamp"
+#define AUTHORS_THREE_B "Andre Beer, Julian Meister, Sophie Roggenkamp"
+#define AUTHORS_THREE_C "Andre Beer, Julian Meister, Sophie Roggenkamp"
+#define AUTHORS_THREE_D "Andre Beer, Julian Meister, Sophie Roggenkamp"
+#define AUTHORS_THREE_E "Andre Beer, Julian Meister, Sophie Roggenkamp"
